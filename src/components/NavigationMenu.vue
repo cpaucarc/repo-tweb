ecd858e8c4367533ae683d0c1a880f60d133676b
Frank <fcpaucar@gmail.com>
2022-03-06 20:47:35 -0500

    style(navbar): se añadió elementos al menú de navegación

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -1,36 +1,83 @@
 <template>
-  <div class="container">
-    <ul class="navbar">
-      <li class="nav-item">
-        <router-link class="nav-link" to="/">Inicio</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/about">Acerca de</router-link>
-      </li>
-    </ul>
+  <div class="nav-menu">
+    <div class="container navbar">
+      <ul class="lista-rutas">
+        <li class="nav-item" v-for="(ruta, i) in rutas" :key="i">
+          <router-link class="nav-link item" :to="ruta.ruta">
+            {{ ruta.nombre }}
+          </router-link>
+        </li>
+      </ul>
+
+      <div>
+        <input type="text" placeholder="Buscar proyectos..." />
+      </div>
+
+      <div class="lista-rutas">
+        <button class="item">+ Subir mi proyecto</button>
+        <button class="item">Login</button>
+        <UsuarioConectado />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import UsuarioConectado from "./Usuario/UsuarioConectado.vue";
+  const rutas = [
+    // Definir mas rutas en el archivo router.js
+    {
+      ruta: "/",
+      nombre: "Inicio",
+    },
+    {
+      ruta: "about",
+      nombre: "Acerca de",
+    },
+  ];
+
   export default {
     name: "NavigationMenu",
+    components: { UsuarioConectado },
+    setup() {
+      return { rutas };
+    },
   };
 </script>
 
 <style scoped>
-  .navbar {
-    column-gap: 1.5rem;
+  .nav-menu {
     align-items: center;
+    background-color: white;
+    display: flex;
+    height: 60px;
+    width: 100%;
+  }
+  .navbar {
+    align-items: center;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+  }
+  .lista-rutas {
+    align-items: center;
+    background-color: white;
+    column-gap: 1.5rem;
     display: flex;
     padding: 0;
   }
   .nav-item {
-    color: black;
     list-style-type: none;
-    appearance: unset;
   }
-
   .nav-link {
+    background-color: white;
+    color: #52525b;
+    margin: auto;
+    text-decoration: none;
+  }
+  .router-link-exact-active {
+    color: #7c43f7;
+    font-weight: bold;
     text-decoration: none;
   }
 </style>
---
+++ src/components/Usuario/UsuarioConectado.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="dropdown-usuario">
+    <div class="trigger" @click="verOpciones = !verOpciones">
+      <img
+        class="foto-perfil"
+        src="https://preview.redd.it/v0caqchbtn741.jpg?auto=webp&s=c5d05662a039c031f50032e22a7c77dfcf1bfddc"
+        alt="Foto de perfil"
+      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        width="16"
+        height="16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M12.78 6.22a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06 0L3.22 7.28a.75.75 0 011.06-1.06L8 9.94l3.72-3.72a.75.75 0 011.06 0z"
+        ></path>
+      </svg>
+    </div>
+
+    <div class="opciones" v-show="verOpciones">
+      <ul>
+        <li><a href="#">Cerrar Sesión</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { ref } from "vue";
+  export default {
+    name: "UsuarioConectado",
+    setup() {
+      const verOpciones = ref(false);
+
+      return { verOpciones };
+    },
+  };
+</script>
+
+<style>
+  .trigger {
+    align-items: center;
+    column-gap: 0.25rem;
+    cursor: pointer;
+    display: flex;
+  }
+  .foto-perfil {
+    border-radius: 999px;
+    height: 35px;
+    object-fit: cover;
+    width: 35px;
+  }
+
+  .dropdown-usuario {
+    position: relative;
+  }
+
+  .opciones {
+    border: 1px solid black;
+    position: absolute;
+    top: 55px;
+    width: 150px;
+  }
+</style>
