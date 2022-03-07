4b85851e43e375a3bac48b6da60174e426537db0
Frank <fcpaucar@gmail.com>
2022-03-07 18:23:23 -0500

    style: se añadió detalles del curso y grupo en /about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,26 @@
 <template>
-  <h1 class="text-primary">Acerca de</h1>
+  <h1>Tecnologia Web - Unasam</h1>
+
+  <br />
+
+  <h3>Grupo</h3>
+  <ol style="margin-left: 25px; margin-top: 10px;">
+    <li>
+      <h4>Blas Obregon Clinton</h4>
+    </li>
+    <li>
+      <h4>Pagola Pajuelo Angel</h4>
+    </li>
+    <li>
+      <h4>Paucar Colonia Frank</h4>
+    </li>
+  </ol>
 </template>
 
 <script>
-  export default {
-    name: "About",
-  };
+export default {
+  name: "About",
+};
 </script>
 
 <style>
